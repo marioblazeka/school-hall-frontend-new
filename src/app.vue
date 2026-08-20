f19bc63893a35cf6ee41d6f7f71a637482772f1e
Mario Blažeka <mblazeka@student.unipu.hr>
2026-08-20 13:48:13 +0200

    commit - Vue 3 i Tailwind ručna konfiguracija

---
+++ src/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="min-h-screen bg-gray-100 flex items-center justify-center">
+    <h1 class="text-3xl font-bold text-blue-600">Tailwind i Vue 3 rade!</h1>
+  </div>
+</template>
